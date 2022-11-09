e308fad28c050dbe632e1713ebf199a94b0aefe0
jamesdupark <james_m_park@brown.edu>
2022-11-09 17:52:48 -0500

    react studio finished!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, ListGroup } from "react-bootstrap";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +18,59 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  let total = 0;
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h1>My Bakery</h1>{" "}
+            {/* TODO: personalize your bakery (if you want) */}
+            <Container>
+              <Row>
+                {bakeryData.map(
+                  (
+                    item,
+                    index // TODO: map bakeryData to BakeryItem components
+                  ) => (
+                    <Col id={index} className="d-flex align-items-stretch">
+                      <BakeryItem
+                        name={item.name}
+                        desc={item.description}
+                        price={item.price}
+                        url={item.image}
+                        handler={setCart}
+                        cart={cart}
+                      />
+                    </Col>
+                  )
+                )}
+              </Row>
+            </Container>
+          </Col>
+          <Col lg={3}>
+            <h1>Cart</h1>
+            <Card>
+              <Card.Body>
+                <ListGroup variant="flush">
+                  {cart.map(({ name, price, qty }) => {
+                    console.log(name, price, qty);
+                    total += price * qty;
+                    return (
+                      <ListGroup.Item>
+                        {qty}x {name}: ${price}
+                      </ListGroup.Item>
+                    );
+                  })}
+                </ListGroup>
+              </Card.Body>
+              <Card.Footer>Total price: ${total}</Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,60 @@
 // TODO: create a component that displays a single bakery item
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function BakeryItem0({ name, desc, price, url }) {
+  const item = (
+    <div className="BakeryItem">
+      <img src={url} alt={name}></img>
+      <h3>{name}</h3>
+      <p>
+        <b>Price: </b>
+        {price}
+      </p>
+      <p>{desc}</p>
+    </div>
+  );
+  return item;
+}
+
+function BakeryItem({ name, desc, price, url, handler, cart }) {
+  const addToCart = () => {
+    console.log(cart);
+    const newItem = { name: name, price: price, qty: 1 };
+    const obj = cart.find((e) => e.name === name);
+    console.log(obj);
+    if (obj !== undefined) {
+      handler(
+        cart.map((e) => {
+          if (e === obj) {
+            return { name: name, price: price, qty: obj.qty + 1 };
+          } else {
+            return e;
+          }
+        })
+      );
+    } else {
+      handler(cart.concat(newItem));
+    }
+  };
+
+  const card = (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={url} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Subtitle>Price: {price}</Card.Subtitle>
+        <Card.Text>{desc}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="d-flex flex-column align-items-end">
+        <Button variant="primary" onClick={addToCart}>
+          Add to Cart
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
+
+  return card;
+}
+
+export default BakeryItem;
